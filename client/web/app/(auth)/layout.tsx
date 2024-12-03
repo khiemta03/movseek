ef5951486954b18019c640147e438fe0a39338c3
tmplam <phulam.dev@gmail.com>
2024-12-03 23:00:05 +0700

    add back button to auth pages

--- client/web/app/(auth)/layout.tsx
+++ client/web/app/(auth)/layout.tsx
@@ -1,8 +1,16 @@
+import { Button } from '@/components/ui/button';
+import { CircleChevronLeft } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex min-h-screen w-full items-center justify-center bg-primary">
+      <Link href="/" className="absolute left-6 top-6">
+        <Button variant="secondary">
+          <CircleChevronLeft /> Back Home
+        </Button>
+      </Link>
       <div className="my-6">{children}</div>
     </main>
   );
