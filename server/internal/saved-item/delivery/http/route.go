a80dda862294bc0d757d895a426602874c740f18
khiemta03 <khiemta.dev@gmail.com>
2025-01-09 23:20:55 +0700

    change methods

--- server/internal/saved-item/delivery/http/route.go
+++ server/internal/saved-item/delivery/http/route.go
@@ -4,6 +4,6 @@ import "github.com/gin-gonic/gin"
 
 func (h handlerImpl) MapRoutes(r *gin.RouterGroup) {
 	r.GET("/saved-item/:user_id", h.getSavedItem)
-	r.POST("/saved-item/add", h.addToSavedItem)
-	r.POST("/saved-item/remove", h.removeFromSavedItem)
+	r.PUT("/saved-item/add", h.addToSavedItem)
+	r.PUT("/saved-item/remove", h.removeFromSavedItem)
 }
